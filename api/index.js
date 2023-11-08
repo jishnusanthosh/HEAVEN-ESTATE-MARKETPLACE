db1c2b31fb9802ee13acf0d56fc648ce0c5a454b
jishnusanthosh <jishnusanthosh321@gmail.com>
2023-11-08 18:26:41 +0530

    database configured

---
+++ api/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Could not connect to MongoDB:', err);
+  });
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
