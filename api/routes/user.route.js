bbd1f993a415a9f87c6d3f2f7b8fb81b20efa6ec
jishnusanthosh <jishnusanthosh321@gmail.com>
2023-11-20 14:03:26 +0530

    get user listing api is completed

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {test, updateUser,deleteUser} from "../controllers/user.controller.js"
+import {test, updateUser,deleteUser,getUserListings} from "../controllers/user.controller.js"
 import { verifyToken } from '../utils/VerifyUser.js';
 
 
@@ -9,6 +9,7 @@ const router =express.Router();
 router.get('/test',test);
 router.post('/update/:id',verifyToken, updateUser)
 router.delete('/delete/:id',verifyToken,deleteUser)
+router.get('/listing/:id',verifyToken,getUserListings)
 
 
 export default router;
