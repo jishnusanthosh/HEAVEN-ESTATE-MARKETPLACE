9a80e317898f0a4c6efd04ba2ea737432e3b1cb6
jishnusanthosh <jishnusanthosh321@gmail.com>
2023-11-20 13:41:16 +0530

    create-listing page is completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import About from "./pages/About"
 import Profile from "./pages/Profile"
 import Header from "./components/Header"
 import PrivateRoute from "./components/PrivateRoute"
+import CreateListing from "./pages/CreateListing"
 
 function App() {
   return (
@@ -20,11 +21,8 @@ function App() {
       <Route path="/sign-out" element={<SignOut />}/>
       <Route path="/about" element={<About />}/>
       <Route  element={<PrivateRoute />}/>
-      <Route path="/profile" element={<Profile />}/>
-
-
-
-      
+        <Route path="/profile" element={<Profile />}/>
+        <Route path="/create-listing" element={<CreateListing />}/>
     </Routes>
 
     </BrowserRouter>
